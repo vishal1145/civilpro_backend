16eb42edcad97d793600a96ebeb2b215cab667c2
vishal laptop <vishal.kumar1145@gmail.com>
2019-02-23 11:03:36 +0530

    a

--- controllers/shared.js
+++ controllers/shared.js
@@ -53,7 +53,7 @@ module.exports = function (app) {
             if (type === "1") {
                 finalQuery = `select u.card_date, e.first_name as emp_first_name, e.last_name as emp_last_name, p.Project_name, u.work_type, 
                 cl.first_name as cli_first_name , cl.last_name as cli_last_name,
-                u.description, u.hours, 'yes' as billed, u.employee_id 
+                u.description, u.hours, 'yes' as billed, u.employee_id ,p.Project_id as project_id
                 from time_card u
             inner join employee e on e.empl_id = u.employee_id
             inner join Project p on p.Project_id = u.project_name
