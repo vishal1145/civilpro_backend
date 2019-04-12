954d1c453712ebd0c2acc85618b66b1688106bd8
vishal1145 <vishal.kumar1145@gmail.com>
2019-04-12 19:09:30 +0530

    push added

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var  lib = process.cwd();
 var UserDevice = require(lib + '/models/UserDevice')
+var PushNotification = require(lib + '/models/PushNotification')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -24,5 +25,36 @@ router.get('/updateUserDevice', async function(req, res){
   res.json(e)
 }
 })
+
+router.get('/add-project-notificaion', async function(req, res){
+  try{
+  let id = Number(req.query.userid);
+  let pid = Number(req.query.projectid);
+  var notifObj = {
+    targetId: id,
+    title: "Admin added you the project",
+    text: "Admin added you the project",
+    type: 'PROJECTUPDATE',
+    isRead: false,
+    refData: {
+        GroupId: pid
+    }
+}
+
+// var notManager = new NotificationManager();
+// notManager.CREATEPUSHNOTIFICATION(notifObj);
+
+var PushNotificationObj = new PushNotification(notobj);
+        PushNotificationObj.save(PushNotificationObj, function (err, logresult) {
+          res.json(e)
+        });
+
+}
+catch(e){
+  res.json(e)
+}
+
+})
+
 
 module.exports = router;
